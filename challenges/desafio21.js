97c5795aff6afd8e77e6389ae1ecaabd5935333e
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-17 14:51:27 -0300

    retorna os sanduiches que possuiem mais curtidas que numeros de vendas

--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,2 +1,4 @@
-const vendidos = db.produtos.find({}, { vendidos: 1, _id: 0 });
-db.produtos.find({ curtidas: { $gt: vendidos } }, { nome: 1, _id: 0 });
+db.produtos.find(
+  { $expr: { $gt: ["$curtidas", "$vendidos"] } }, 
+  { nome: 1, _id: 0 },
+);
